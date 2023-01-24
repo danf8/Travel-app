e703c228b3d90be139de871a0a21eb70b6e2507a
Daniel Fields <dgfields4193@gmail.com>
2023-01-24 11:43:43 -0500

    wip: define index and create routes

--- server.js
+++ server.js
@@ -25,8 +25,32 @@ db.on('error', (err) => {
 });
 
 //mount middleware
+app.use(express.urlencoded({extended: false}));
 
 //mount routes
+//INDUCES
+
+//seed data
+
+//Index
+app.get('/locations', (req, res) => {
+    res.send('locations page');
+});
+
+//New
+
+//Delete
+
+//Update
+
+//Create
+app.post('/locations', (req, res) => {
+    res.send(req.body);
+});
+
+//Edit
+
+// Show- 
 
 //tell the application to listen on a dedicated port.
 app.listen(PORT, () => {
