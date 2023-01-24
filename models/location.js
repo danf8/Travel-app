728ef1fffb0de29c04e4995419213b7f09c6b78c
Daniel Fields <dgfields4193@gmail.com>
2023-01-24 12:25:38 -0500

    wip: adds location model

---
+++ models/location.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const locationSchema = new Schema({
+    name: {type: String, required: true},
+    img: String,
+    description: String,
+    hotels: String,
+    attractions: String,
+});
+
+module.exports = mongoose.model('location', locationSchema);
