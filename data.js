8ddd6736cbc8403ef0688c14aca6dfdff9c1c6fe
Daniel Fields <dgfields4193@gmail.com>
2023-01-24 13:20:26 -0500

    wip: add seed data

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [{
+    name: 'Playa del Carmen',
+    img: 'https://images.pexels.com/photos/12168652/pexels-photo-12168652.jpeg?auto=compress&cs=tinysrgb&w=800',
+    description: "Costal resourt town along the Caribbean shoreline",
+    hotels: 'Hyatt',
+    attractions: 'Mostly beaches',
+},{
+    name: 'Aruba',
+    img: 'https://i.imgur.com/94oOjcw.png',
+    description: 'Island in the southern Caribbean',
+    hotels: 'Marriott Renaissance ',
+    attractions: 'Flamingo Beach',
+},
+{
+    name: 'South Korea',
+    img: 'https://images.pexels.com/photos/2376712/pexels-photo-2376712.jpeg?auto=compress&cs=tinysrgb&w=800',
+    description: 'Country located on southern half of Korean Peninsula, surrounded by Sea of Japan, Yellow Sea, and East China Sea ',
+    hotels: 'Hyatt',
+    attractions: 'Gyeongbokgung Palace',
+},]
