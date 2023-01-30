71af68e979eb84e036a0bc2ca9026ceb63043141
Daniel Fields <dgfields4193@gmail.com>
2023-01-30 13:37:02 -0500

    wip: install jquery

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+
+const logout = document.querySelector('.logout');
+const profile = document.querySelector('.profile');
+
+$('svg').on('mouseenter', function(){
+    logout.style.opacity = 1
+})
+
+$('nav').on('mouseleave', function() {
+    logout.style.opacity = 0
+})
+
+
+
